b4f2f9459ffba72ced03c73e1158466b97503512
Julia Samól <julia.samol@gmail.com>
2019-05-13 00:09:37 +0200

    Finish obtaining statistics when library returns controll to the application

--- app/src/main/java/pl/edu/agh/bioauthdemo/ui/facerecognition/AuthenticateFaceActivity.kt
+++ app/src/main/java/pl/edu/agh/bioauthdemo/ui/facerecognition/AuthenticateFaceActivity.kt
@@ -1,10 +1,11 @@
 package pl.edu.agh.bioauthdemo.ui.facerecognition
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_authenticate_face.*
 import pl.edu.agh.bioauth.auth.listener.AuthenticationListener
+import pl.edu.agh.bioauth.stats.StatsUtil
 import pl.edu.agh.bioauthdemo.R
 import pl.edu.agh.bioauthdemo.util.AppConfig
 
@@ -16,10 +17,12 @@ class AuthenticateFaceActivity : AppCompatActivity() {
 
         faceAuthenticationView.initAuthentication(null, object : AuthenticationListener {
             override fun onSuccess(userId: String, challenge: String, signedChallenge: String) {
+                StatsUtil.onEnd()
                 Toast.makeText(this@AuthenticateFaceActivity, "Authenticated", Toast.LENGTH_SHORT).show()
             }
 
             override fun onFailure(error: Throwable) {
+                StatsUtil.onEnd()
                 Toast.makeText(this@AuthenticateFaceActivity, "Authentication failed.", Toast.LENGTH_SHORT).show()
             }
         }, AppConfig.livenessMode)
--- app/src/main/java/pl/edu/agh/bioauthdemo/ui/facerecognition/RegisterFaceActivity.kt
+++ app/src/main/java/pl/edu/agh/bioauthdemo/ui/facerecognition/RegisterFaceActivity.kt
@@ -1,10 +1,11 @@
 package pl.edu.agh.bioauthdemo.ui.facerecognition
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_register_face.*
 import pl.edu.agh.bioauth.auth.listener.RegistrationListener
+import pl.edu.agh.bioauth.stats.StatsUtil
 import pl.edu.agh.bioauthdemo.R
 import pl.edu.agh.bioauthdemo.util.AppConfig
 import java.security.PublicKey
@@ -17,10 +18,12 @@ class RegisterFaceActivity : AppCompatActivity() {
 
         faceRegistrationView.initRegistration("testUser", object : RegistrationListener {
             override fun onSuccess(publicKey: PublicKey) {
+                StatsUtil.onEnd()
                 Toast.makeText(this@RegisterFaceActivity, "Samples registered.", Toast.LENGTH_SHORT).show()
             }
 
             override fun onFailure(error: Throwable) {
+                StatsUtil.onEnd()
                 Toast.makeText(this@RegisterFaceActivity, "Samples could not be registered.", Toast.LENGTH_SHORT).show()
             }
         }, AppConfig.livenessMode)
